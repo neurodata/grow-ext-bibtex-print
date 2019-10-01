17139efa5f19beac0cedb29c06a00ec102ea0415
Ben Falk <falk.ben@gmail.com>
2019-10-01 13:10:43 -0400

    All authors (#2)
    * formatting
    
    explicitly add jinja2 as dependency
    
    increment version
    
    use requirements.txt file
    
    * add all_auths and auth_formatted methods
    
    remove unused import
    
    * wrong filter

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+from os import path
 from setuptools import setup
 
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "requirements.txt")) as f:
+    required = f.read().splitlines()
+
+
 setup(
-    name='grow-ext-bibtex-print',
-    version='0.0.6',
-    license='MIT',
-    author='Ben Falk',
-    author_email='falk.ben@gmail.com',
+    name="grow-ext-bibtex-print",
+    version="0.0.7",
+    license="MIT",
+    author="Ben Falk",
+    author_email="falk.ben@gmail.com",
     include_package_data=False,
-    packages=[
-        'bibtex_print',
-    ],
-    install_requires=[
-        'bibtexparser==1.1.0',
-    ],
+    packages=["bibtex_print"],
+    install_requires=required,
 )
