f08367f00582bf119cfeae31acf5a00e0ebe712e
Ben Falk <falk.ben@gmail.com>
2019-10-01 11:57:38 -0400

    formatting
    explicitly add jinja2 as dependency
    
    increment version
    
    use requirements.txt file

--- bibtex_print/bibtex_print.py
+++ bibtex_print/bibtex_print.py
@@ -9,8 +9,55 @@ from jinja2.ext import Extension
 
 def load_bibtex(bibfile):
     def mo_co(mo):
-        MONTH_CONVERT = {'': 0, 'jan': 1, 'Jan': 1, 'january': 1, 'January': 1, 'feb': 2, 'Feb': 2, 'february': 2, 'February': 2, 'mar': 3, 'Mar': 3, 'march': 3, 'March': 3, 'apr': 4, 'Apr': 4, 'april': 4, 'April': 4, 'may': 5, 'May': 5, 'jun': 6, 'June': 6, 'june': 6, 'June': 6, 'jul': 7, 'Jul': 7, 'july': 7, 'July': 7,
-                         'aug': 8, 'Aug': 8, 'august': 8, 'August': 8, 'sep': 9, 'Sep': 9, 'september': 9, 'September': 9, 'oct': 10, 'Oct': 10, 'october': 10, 'October': 10, 'nov': 11, 'Nov': 11, 'november': 11, 'November': 11, 'dec': 12, 'Dec': 12, 'december': 12, 'December': 12}
+        MONTH_CONVERT = {
+            "": 0,
+            "jan": 1,
+            "Jan": 1,
+            "january": 1,
+            "January": 1,
+            "feb": 2,
+            "Feb": 2,
+            "february": 2,
+            "February": 2,
+            "mar": 3,
+            "Mar": 3,
+            "march": 3,
+            "March": 3,
+            "apr": 4,
+            "Apr": 4,
+            "april": 4,
+            "April": 4,
+            "may": 5,
+            "May": 5,
+            "jun": 6,
+            "June": 6,
+            "june": 6,
+            "June": 6,
+            "jul": 7,
+            "Jul": 7,
+            "july": 7,
+            "July": 7,
+            "aug": 8,
+            "Aug": 8,
+            "august": 8,
+            "August": 8,
+            "sep": 9,
+            "Sep": 9,
+            "september": 9,
+            "September": 9,
+            "oct": 10,
+            "Oct": 10,
+            "october": 10,
+            "October": 10,
+            "nov": 11,
+            "Nov": 11,
+            "november": 11,
+            "November": 11,
+            "dec": 12,
+            "Dec": 12,
+            "december": 12,
+            "December": 12,
+        }
 
         return MONTH_CONVERT[mo]
 
@@ -22,71 +69,71 @@ def load_bibtex(bibfile):
 
     bib_entries = bib_database.entries
 
-    bib_entries.sort(key=lambda x: x.get('author', ''))
-    bib_entries.sort(key=lambda x: mo_co(x.get('month', '')), reverse=True)
-    bib_entries.sort(key=lambda x: x.get('year', ''), reverse=True)
+    bib_entries.sort(key=lambda x: x.get("author", ""))
+    bib_entries.sort(key=lambda x: mo_co(x.get("month", "")), reverse=True)
+    bib_entries.sort(key=lambda x: x.get("year", ""), reverse=True)
 
     return bib_entries
 
 
 def print_authors(bib_entry):
-    authors = bib_entry['author'].split(' and ')
+    authors = bib_entry["author"].split(" and ")
 
     auths_format = []
     for author in authors:
-        names = author.split(', ')
+        names = author.split(", ")
         last = names[0]
-        if last[0] == '{' and last[-1] == '}':
+        if last[0] == "{" and last[-1] == "}":
             last = last[1:-1]
         initials = []
         if len(names) > 1:
-            for n in names[1].split(' '):
+            for n in names[1].split(" "):
                 if len(n) > 1:
-                    initials += n[0] + '. '
+                    initials += n[0] + ". "
                 else:
-                    initials += n + '. '
-        full_name_format = ''.join(initials) + last
+                    initials += n + ". "
+        full_name_format = "".join(initials) + last
         auths_format.append(full_name_format)
 
     # return et al if # of auths_format > 3
     if len(auths_format) > 3:
-        return auths_format[0] + ' et al.'
+        return auths_format[0] + " et al."
     elif len(auths_format) == 1:
-        return auths_format[0] + '.'
+        return auths_format[0] + "."
     else:
-        return u'{} and {}.'.format(', '.join(auths_format[0:-1]), auths_format[-1])
+        return u"{} and {}.".format(", ".join(auths_format[0:-1]), auths_format[-1])
 
 
 def print_link(bib_entry):
-    if 'doi' in bib_entry:
-        return 'https://doi.org/' + bib_entry['doi']
-    if 'url' in bib_entry:
-        return bib_entry['url']
-    if 'arxivid' in bib_entry:
-        arxivID = bib_entry['arxivid'].replace('arXiv:', '')
-        return 'https://arxiv.org/abs/{}'.format(arxivID)
-    print('{} did not have a link'.format(bib_entry['ID']))
+    if "doi" in bib_entry:
+        return "https://doi.org/" + bib_entry["doi"]
+    if "url" in bib_entry:
+        return bib_entry["url"]
+    if "arxivid" in bib_entry:
+        arxivID = bib_entry["arxivid"].replace("arXiv:", "")
+        return "https://arxiv.org/abs/{}".format(arxivID)
+    print("{} did not have a link".format(bib_entry["ID"]))
     # print('here are the keys')
     # for key in bib_entry:
     #     print(key)
 
 
 def print_venue(bib_entry):
-    if 'journal' in bib_entry:
-        return bib_entry['journal']
+    if "journal" in bib_entry:
+        return bib_entry["journal"]
 
     # probably a conference
-    if 'booktitle' in bib_entry:
-        return bib_entry['booktitle']
+    if "booktitle" in bib_entry:
+        return bib_entry["booktitle"]
 
 
 def print_title(bib_entry):
-    title = bib_entry['title']
+    title = bib_entry["title"]
 
-    if title[0] == '{' and title[-1] == '}':
+    if title[0] == "{" and title[-1] == "}":
         title = title[1:-1]
 
-    if '\href' in title:
+    if "\href" in title:
         m = re.findall(r"\\href\{(.+)\}{(.+)}", title)
         link = m[0][0]
         t = m[0][1]
@@ -96,24 +143,24 @@ def print_title(bib_entry):
 
 
 def print_issue_data(bib_entry):
-    if 'pages' in bib_entry and 'number' in bib_entry and 'volume' in bib_entry:
+    if "pages" in bib_entry and "number" in bib_entry and "volume" in bib_entry:
         return print_issue(bib_entry) + print_volume(bib_entry) + print_pages(bib_entry)
     else:
-        return ''
+        return ""
 
 
 def print_pages(bib_entry):
-    pages = bib_entry['pages']
-    pages = pages.replace('--', '-')
-    return ':' + pages + ', '
+    pages = bib_entry["pages"]
+    pages = pages.replace("--", "-")
+    return ":" + pages + ", "
 
 
 def print_volume(bib_entry):
-    return '{}'.format(bib_entry['volume'])
+    return "{}".format(bib_entry["volume"])
 
 
 def print_issue(bib_entry):
-    return '({})'.format(bib_entry['number'])
+    return "({})".format(bib_entry["number"])
 
 
 def print_current_year(_):
@@ -121,13 +168,12 @@ def print_current_year(_):
 
 
 class BIBTEX_PRINT(Extension):
-
     def __init__(self, environment):
         super(BIBTEX_PRINT, self).__init__(environment)
-        environment.filters['load_bibtex'] = load_bibtex
-        environment.filters['print_authors'] = print_authors
-        environment.filters['print_link'] = print_link
-        environment.filters['print_title'] = print_title
-        environment.filters['print_venue'] = print_venue
-        environment.filters['print_issue_data'] = print_issue_data
-        environment.filters['print_current_year'] = print_current_year
+        environment.filters["load_bibtex"] = load_bibtex
+        environment.filters["print_authors"] = print_authors
+        environment.filters["print_link"] = print_link
+        environment.filters["print_title"] = print_title
+        environment.filters["print_venue"] = print_venue
+        environment.filters["print_issue_data"] = print_issue_data
+        environment.filters["print_current_year"] = print_current_year
